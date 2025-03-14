9e1c3b46807113bab01514038bd88f816e61d3bc
ullas-kiran <ullas261@gmail.com>
2025-03-14 14:19:22 +0530

    product card

--- client/src/pages/shopping/listing.jsx
+++ client/src/pages/shopping/listing.jsx
@@ -31,6 +31,9 @@ const ShoppingListing = () => {
               </DropdownMenu>
             </div>
           </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+              
+          </div>
        </div>
     </div>
   )
