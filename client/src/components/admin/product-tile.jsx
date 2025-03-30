ee462ac1037e29870e694538fad6bd89c2a12228
ullas-kiran <ullas261@gmail.com>
2025-03-30 12:19:59 +0530

    fix(db):Schema mistake fix

--- client/src/components/admin/product-tile.jsx
+++ client/src/components/admin/product-tile.jsx
@@ -10,10 +10,12 @@ const AdminProductTile = ({product}) => {
           <img  src={product?.image} alt={product?.title} className="w-full h-[300px] object-cover rounded-t-lg"/>
         </div>
         <CardContent>
-        <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
+        <h2 className="text-xl font-bold mb-2 mt-2">{product?.title}</h2>
         <div className="flex justify-between items-center mb-2">
-            <span className={`${product?.salePrice>0?'line-through':''}text-lg font-semibold text-primary`}>{product?.price}</span>
-             <span className="text-lg font-bold">{product?.salePrice}</span>
+            <span className={`${product?.salePrice>0?'line-through':''}text-lg font-semibold text-primary`}>${product?.price}</span>
+            {
+                product?.salePrice>0?<span className="text-lg font-bold">${product?.salePrice}</span>:null
+            }
         </div>
         </CardContent> 
         <CardFooter className="flex justify-between items-center">
