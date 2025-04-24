b4245c22872cf82240ad087ca8155c8a588f2ba1
ullas-kiran <ullas261@gmail.com>
2025-04-24 15:37:13 +0530

    delete product api

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -100,7 +100,21 @@ const editProduct=async(req,res)=>{
 
 const deleteProduct=async(req,res)=>{
       try {
-        
+        const {id} = req.params;
+        const product= await Product.findByIdAndUpdate(id);
+
+         if(!product){
+            return res.status(404).json({
+                success:false,
+                message:"Product not found"
+            })
+          }
+
+          res.status(200).json({
+            success:true,
+            message:'Product delete successfully'
+          })
+
     } catch (error) {
         res.status(500).json({
             success:false,
