aa70a7816eb7c362ef9d1a921b2e482ef9be61ad
ullas-kiran <ullas261@gmail.com>
2025-05-11 22:50:34 +0530

    auth fix

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -97,8 +97,25 @@ res.clearCookie('token').json({
 
 // auth middleware
 
-const authMiddleware=()=>{
+const authMiddleware=async(req,res,next)=>{
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorised user!",
+    });
+  }
 
+  try {
+    const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({
+        success:false,
+        message:"Unauthorised user!"
+    })
+  }
 }
 
-module.exports={registerUser,loginUser,logout}
+module.exports={registerUser,loginUser,logout,authMiddleware}
