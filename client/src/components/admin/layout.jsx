9eb5c2b9b21898be76203e71a486a5f21f6b56a0
ullas-kiran <ullas261@gmail.com>
2025-05-15 23:44:56 +0530

    fix(sidebar):style changes

--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import AdminSidebar from './sidebar'
 import AdminHeader from './header'
+import { useState } from 'react'
 
 const AdminLayout = () => {
+  const [openSidebar,setOpenSidebar]=useState(false)
   return (
     <div className='flex min-h-screen w-full'>
-        <AdminSidebar/>
+        <AdminSidebar open={openSidebar} setOpen={setOpenSidebar}/>
        <div className='flex flex-1 flex-col'>
-        <AdminHeader/>
+        <AdminHeader setOpen={setOpenSidebar}/>
           <main className='flex flex-1 bg-muted/40 p-4 md:p-6'>
             <Outlet/>
           </main>
--- client/src/components/admin/sidebar.jsx
+++ client/src/components/admin/sidebar.jsx
@@ -1,62 +1,83 @@
-import { ChartNoAxesCombined, LayoutDashboard, PackageSearch, ShoppingBasket } from "lucide-react"
-import { Fragment } from "react"
-import { useNavigate } from "react-router-dom"
+import {
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  PackageSearch,
+  ShoppingBasket,
+} from "lucide-react";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
-
- const adminSidebarMenuItems=[
+const adminSidebarMenuItems = [
   {
-    id:'dashboard',
-    label:'Dashboard',
-    path:'/admin/dashboard',
-    icon:<LayoutDashboard />
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
   },
   {
-    id:'products',
-    label:'Products',
-    path:'/admin/products',
-    icon:<ShoppingBasket />
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
   },
   {
-    id:'orders',
-    label:'Orders',
-    path:'/admin/orders',
-    icon:<PackageSearch />
-  }
-]
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <PackageSearch />,
+  },
+];
 
-const AdminSidebar = () => {
-
-
-  const navigate=useNavigate();
-
-  function MenuItems(){
-
-
-    return <nav className="mt-8 flex-col flex gap-2">
-       {
-        adminSidebarMenuItems.map((menuItem,index)=>{
-          return (
-            <div key={menuItem.id} onClick={() => navigate(menuItem.path)} className="flex text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer">
-              {menuItem.icon}
-              <span>{menuItem.label}</span>
-            </div>
-          )
-        })
-       }
+function MenuItems() {
+  return (
+    <nav className="mt-8 flex-col flex gap-2">
+      {adminSidebarMenuItems.map((menuItem, index) => {
+        return (
+          <div
+            key={menuItem.id}
+            onClick={() => navigate(menuItem.path)}
+            className="flex text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer"
+          >
+            {menuItem.icon}
+            <span>{menuItem.label}</span>
+          </div>
+        );
+      })}
     </nav>
-  }
+  );
+}
 
-    return (
-      <Fragment>
-        <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
-           <div onClick={()=>navigate('/admin/dashboard')} className="flex cursor-pointer items-center gap-2">
-           <ChartNoAxesCombined size={30} />
-              <h1 className="text-3xl font-extrabold">Admin Panel</h1>
-           </div>
-           <MenuItems/>
-        </aside>
-      </Fragment>
-    )
-  }
-  
-  export default AdminSidebar
+const AdminSidebar = ({ open, setOpen }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className={"w-64"}>
+          <div className="flex flex-col h-full">
+            <SheetHeader className={"border-b"}>
+              <SheetTitle>
+                <ChartNoAxesCombined size={30} />
+                Admin Panel
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems/>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          onClick={() => navigate("/admin/dashboard")}
+          className="flex cursor-pointer items-center gap-2"
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+        </div>
+        <MenuItems />
+      </aside>
+    </Fragment>
+  );
+};
+
+export default AdminSidebar;
