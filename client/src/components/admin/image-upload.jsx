7448b0864a9d9f6f9f7ec0de82963282d042ac3d
ullas-kiran <ullas261@gmail.com>
2025-05-19 21:41:25 +0530

    image upload drag

--- client/src/components/admin/image-upload.jsx
+++ client/src/components/admin/image-upload.jsx
@@ -1,7 +1,8 @@
 import { useRef } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { UploadCloudIcon } from "lucide-react";
+import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 const ProductImageUpload = ({imageFile,setImageFile,uploadedImageUrl,setUploadedImageUrl}) => {
 
@@ -15,10 +16,30 @@ const ProductImageUpload = ({imageFile,setImageFile,uploadedImageUrl,setUploaded
     }
   }
 
+  function handleDragOver(event){
+  event.preventDefault()
+
+  }
+
+  function handleDrag(event){
+ event.preventDefault();
+ const droppedFile=event.dataTransfer.files?.[0];
+ if(droppedFile){
+  setImageFile(droppedFile)
+ }
+  }
+
+  function handleRemoveImage(){
+    setImageFile(null);
+    if(inputRef.current){
+      inputRef.current.value=""
+    }
+  }
+
   return (
     <div className="w-full max-w-md mx-auto mt-4">
       <Label className={"text-lg font-semibold mb-2 block"}>Upload Image</Label>
-      <div className="border-2 border-dashed rounded-lg p-4">
+      <div onDragOver={handleDragOver} onDrag={handleDrag} className="border-2 border-dashed rounded-lg p-4">
         <Input id="img-upload" type={"file"} 
         className={"hidden"}
          ref={inputRef} onChange={handleImageFileChange}/>
@@ -27,8 +48,16 @@ const ProductImageUpload = ({imageFile,setImageFile,uploadedImageUrl,setUploaded
          <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2"/>
          <span>Drag and Drop or clicked to upload image</span>
           </Label>
-         :<div>
-          </div>}
+         :<div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <FileIcon className="w-8 text-primary mr-2 h-8"/>
+            </div> 
+            <p className="text-sm font-medium">{imageFile.name}</p>
+            <Button variant={"ghost"} size={"icon"} className={"text-muted-foreground hover:text-foreground"} onClick={handleRemoveImage}>
+              <XIcon className="w-4 h-4"/>
+              <span className="">Remove File</span>
+            </Button>
+            </div>}
       </div>
     </div>
   );
