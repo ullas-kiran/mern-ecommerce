77ec676c5288db75e06793902cc96985eda9073d
ullas-kiran <ullas261@gmail.com>
2025-05-23 17:22:01 +0530

    users header

--- client/src/components/shopping/header.jsx
+++ client/src/components/shopping/header.jsx
@@ -1,8 +1,30 @@
+import { HousePlug, Menu } from "lucide-react"
+import { Link } from "react-router-dom"
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet"
+import { Button } from "../ui/button"
 
 
 const ShoppingHeader = () => {
   return (
-    <div>ShoppingHeader</div>
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+     <div className="flex h-16 items-center justify-between px-4 md:px-6">
+      <Link to={'/shop/home'} className="flex items-center gap-2">
+      <HousePlug className="h-6 w-6"/>
+      <span className="font-bold">Ecommerce</span>
+      </Link>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant={'outline'} size={'icon'} className={`lg:hidden`}>
+            <Menu className="h-6 w-6"/> 
+            <span className="sr-only">Toggle header menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={'left'} className={'w-full max-w-xs'}>
+
+        </SheetContent>
+      </Sheet>
+     </div>
+    </header>
   )
 }
 
