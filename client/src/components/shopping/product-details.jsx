b6e75cd22eb8668cca3e10f21680092c98989f36
ullas-kiran <ullas261@gmail.com>
2025-06-03 23:47:37 +0530

    modal fix

--- client/src/components/shopping/product-details.jsx
+++ client/src/components/shopping/product-details.jsx
@@ -4,7 +4,7 @@ import { Dialog, DialogContent } from '../ui/dialog'
 const ProductDetailsDialog = ({open,setOpen}) => {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className={'grid grid-cols-2 gap-8'}>
+      <DialogContent className={'grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]'}>
 
       </DialogContent>
 
