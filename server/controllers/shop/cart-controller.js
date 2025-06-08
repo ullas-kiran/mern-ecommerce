3a8231d501781c68cc1fa06d7c219babfd18a683
ullas-kiran <ullas261@gmail.com>
2025-06-08 13:58:40 +0530

    fix in cart

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -1,5 +1,47 @@
+const Cart=require('../../models/cart');
+const Product=require('../../models/product')
+
 const addToCart=async(req,res)=>{
     try {
+
+     const {userId,productId,quantity}=req.body;
+
+     if(!userId || !productId || quantity<=0){
+       return res.status(400).json({
+        success:false,
+        message:"Invalid data provided!"
+       })
+     }
+
+     const product=await Product.findById(productId);
+
+     if(!product){
+        return res.status(404).json({
+            success:false,
+            message:"Product not found"
+        })
+      }
+
+      let cart=await Cart.findOne({userId});
+
+      if(!cart){
+        cart = new Cart({userId,items:[]});
+      }
+
+      const findCurrentProductIndex=cart.items.findIndex(item=>item.productId.toString() === productId);
+
+      if(findCurrentProductIndex ===-1 ){
+        cart.items.push({productId,quantity});
+      }else{
+         cart.items[findCurrentProductIndex].quantity += quantity;   
+      }
+
+      await cart.save();
+
+      res.status(200).json({
+        success:true,
+        data:cart
+      })
         
     } catch (error) {
         console.log(error);
