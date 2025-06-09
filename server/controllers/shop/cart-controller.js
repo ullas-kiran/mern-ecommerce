6548f5d4fa8fbb09007d4b7a24170a228e250c9a
ullas-kiran <ullas261@gmail.com>
2025-06-09 19:25:44 +0530

    feat(cart):update cart

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -107,7 +107,58 @@ const fetchCartItems=async(req,res)=>{
 
 const updateCartItemQty=async(req,res)=>{
     try {
-        
+            const {userId,productId,quantity}=req.body;
+
+     if(!userId || !productId || quantity<=0){
+       return res.status(400).json({
+        success:false,
+        message:"Invalid data provided!"
+       })
+     }
+
+     const cart = await Cart.findOne({userId})
+
+         if(!cart){
+            return res.status(404).json({
+                success:false,
+                message:"Cart not found"
+            })
+        }
+
+        const findCurrentProductIndex=cart.items.findIndex(item=>item.productId.toString()===productId);
+
+        if(findCurrentProductIndex ===-1 ){
+            return res.status(404).json({
+                success:false,
+                message:"Cart item not present!"
+            })
+          }
+            
+        cart.items[findCurrentProductIndex].quantity=quantity;
+        await cart.save();
+
+        await cart.populate({
+            path:'item.productId',
+            select:'image title price salePrice'
+        })
+
+        const populateCartItems=cart.items.map(item=>({
+            productId:item.productId?item.productId._id:null,
+            title:item.title?item.productId.title:'product not found',
+            image:item.image?item.productId.image:null,
+            price:item.price?item.productId.price:null,
+            salePrice:item.salePrice?item.productId.salePrice:null,
+            quantity:item.quantity
+          }))
+
+          res.status(200).json({
+            success:true,
+            data:{
+                ...cart._doc,
+                items:populateCartItems
+            }
+          })
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
