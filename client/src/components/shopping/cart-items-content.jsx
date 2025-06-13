c767defdbfbcce6949c956315bffac0ea7402ca1
ullas-kiran <ullas261@gmail.com>
2025-06-13 17:43:17 +0530

    delete cart

--- client/src/components/shopping/cart-items-content.jsx
+++ client/src/components/shopping/cart-items-content.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Button } from '../ui/button';
 import { Minus, Plus, Trash } from 'lucide-react';
+import { useDispatch } from 'react-redux';
+import { deleteCartItem } from '@/store/shop/cart-slice';
 
 const UserCartItemsContent = ({cartItem}) => {
-  console.log(cartItem,"cartItems");
+  const {user} = useSelector(state=>state.auth);
+  const dispatch=useDispatch()
+  
+  function handleCartItemDelete(getCartItem){
+    dispatch(deleteCartItem({userId:user?.id,productId:getCartItem?.productId}))
+  }
+
   return (
     <div className="flex items-center space-x-4">
        <img src={cartItem?.image} alt={cartItem?.title} className="w-20 h-20 object-cover" />
-       <div className="flex-1">
+       <dilv className="flex-1">
         <h3 className="font-extrabold">{cartItem?.title}</h3>
         <div className="flex items-center mt-1 gap-2">
           <Button variant={'outline'} size={'icon'} className={'h-8 w-8 rounded-full'}>
@@ -20,12 +28,12 @@ const UserCartItemsContent = ({cartItem}) => {
             <span className="sr-only">Increase</span>
           </Button>
         </div>
-       </div>
+       </dilv>
        <div className="flex flex-col items-end">
         <p className="font-semibold">
           ${((cartItem?.salePrice > 0?cartItem?.salePrice:cartItem?.price) * cartItem?.quantity).toFixed(2)}
         </p>
-        <Trash className="cusror-pointer mt-1"  size={20}/>
+        <Trash onClick={()=>handleCartItemDelete(cartItem)} className="cusror-pointer mt-1"  size={20}/>
        </div>
     </div>
   )
