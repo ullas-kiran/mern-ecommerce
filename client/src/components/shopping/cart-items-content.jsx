5ec86daf4e85fa2223b6f968e868e469447708be
ullas-kiran <ullas261@gmail.com>
2025-06-14 22:21:29 +0530

    toast msg added

--- client/src/components/shopping/cart-items-content.jsx
+++ client/src/components/shopping/cart-items-content.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import { Button } from '../ui/button';
 import { Minus, Plus, Trash } from 'lucide-react';
 import { useDispatch } from 'react-redux';
-import { deleteCartItem } from '@/store/shop/cart-slice';
+import { deleteCartItem, updateCartQuantity } from '@/store/shop/cart-slice';
+import { toast } from 'sonner';
 
 const UserCartItemsContent = ({cartItem}) => {
   const {user} = useSelector(state=>state.auth);
   const dispatch=useDispatch()
+
+  function  handleUpdateQuantity(getCartItem,typeOfAction){
+     dispatch(updateCartQuantity({
+      userId:user?.id,
+      productId:getCartItem?.productId,
+      quantity:typeOfAction==='add'?getCartItem?.quantity-1:getCartItem?.quantity+1 
+     })).then(data=>{
+      if(data?.payload?.success){
+        toast.success('Cart updated successfully')
+      }
+     })
+  }
   
   function handleCartItemDelete(getCartItem){
-    dispatch(deleteCartItem({userId:user?.id,productId:getCartItem?.productId}))
+    dispatch(deleteCartItem({userId:user?.id,productId:getCartItem?.productId})).then(data=>{
+      if(data?.payload?.success){
+        toast.success('Cart deleted successfully')
+      }
+     })
   }
 
   return (
@@ -18,12 +35,12 @@ const UserCartItemsContent = ({cartItem}) => {
        <dilv className="flex-1">
         <h3 className="font-extrabold">{cartItem?.title}</h3>
         <div className="flex items-center mt-1 gap-2">
-          <Button variant={'outline'} size={'icon'} className={'h-8 w-8 rounded-full'}>
+          <Button variant={'outline'} size={'icon'} className={'h-8 w-8 rounded-full'} onClick={()=>handleUpdateQuantity(cartItem,'minus')}>
             <Minus className="w-4 h-4" />
             <span className="sr-only">Decrease</span>
           </Button>
           <span className="font-semibold">{cartItem?.quantity}</span>
-            <Button variant={'outline'} size={'icon'} className={'h-8 w-8 rounded-full'}>
+            <Button variant={'outline'} size={'icon'} className={'h-8 w-8 rounded-full'} onClick={()=>handleUpdateQuantity(cartItem,'plus')}>
             <Plus className="w-4 h-4" />
             <span className="sr-only">Increase</span>
           </Button>
