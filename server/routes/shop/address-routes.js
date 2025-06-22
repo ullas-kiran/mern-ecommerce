3aa72e1a56eb776a45d0239f2b55c1ced1cf003c
ullas-kiran <ullas261@gmail.com>
2025-06-22 23:28:11 +0530

    address routes update

---
+++ server/routes/shop/address-routes.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const {addAddress,deleteAddress,fetchAllAddress,editAddress} =require('../../controllers/shop/address-controller');
+const router=express.Router();
+
+router.post(`/add`,addAddress);
+router.get(`/get/:userId`,fetchAllAddress);
+router.delete(`/delete/:userId/:addressId`,deleteAddress);
+router.put(`/update/:userId`,editAddress);
