c82d923883cfbdc5b253fb53894b83bf6b5d3544
ullas-kiran <ullas261@gmail.com>
2025-06-25 18:04:29 +0530

    address controller

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,6 +1,14 @@
+const Address=require('../../models/address')
+
 const addAddress=async(req,res)=>{
     try {
-        
+     const {userId,address,city,pincode,phone,notes} = req.body;
+     if(!userId||!address||!city||!pincode||!phone||!notes){
+        return res.status(400).json({
+            success:false,
+            message:'Invalid data provided!'
+        })
+     }   
     } catch (error) {
         console.log(error);
         res.status(500).json({
