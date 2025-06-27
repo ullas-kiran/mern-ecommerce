4338f9f33ec9a589d02754ef6a57905359cacc6a
ullas-kiran <ullas261@gmail.com>
2025-06-27 17:37:12 +0530

    tab component

--- client/src/pages/shopping/account.jsx
+++ client/src/pages/shopping/account.jsx
@@ -1,4 +1,5 @@
 import accImg from "@/assets/account.jpg"
+import { Tabs } from "@/components/ui/tabs"
 
 const ShoppingAccount = () => {
   return (
@@ -7,7 +8,14 @@ const ShoppingAccount = () => {
         <img src={accImg}
          className="h-full w-full object-cover object-center"
         />
-        </div>      
+        </div> 
+        <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+          <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+           <Tabs defaultValues="orders">
+
+           </Tabs>
+          </div>
+        </div>     
     </div>
   )
 }
